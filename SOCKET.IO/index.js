c6a13f2e95d06f38d4776d695ac5eefee9993615
TD <trongdoh@gmail.com>
2023-08-02 20:57:43 -0700

    simple web based chat

--- SOCKET.IO/index.js
+++ SOCKET.IO/index.js
@@ -2,14 +2,26 @@ const express = require("express");
 const http = require ("http");
 const socketIO = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 const io = socketIO(server);
 
-app.get('/', (req, res) =>{
-    res.send("Hello World!")
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname +"/index.html");
 });
 
-io.onconnection('connection' , (socket) => {
+io.on("connection" , (socket) => {
     console.log("new User connected");
+
+    socket.on("chat message", (msg) =>{
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
---
+++ SOCKET.IO/readme.txt
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=u7XqDBC9BiI
